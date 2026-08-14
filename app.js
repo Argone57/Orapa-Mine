9fc804bb0f9737dc293c4038d8be00dc233e468b
Argone <altirax@hotmail.com>
2026-08-14 09:25:02 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Orapa Mine V2 - correctif fenêtre de score et classements globaux - 2026-07-25
-const APP_VERSION = '20260814-0007';
+const APP_VERSION = '20260814-0008';
 let publishedAppVersion = null;
 let lastVersionCheckAt = 0;
 let versionCheckPromise = null;
